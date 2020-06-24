d149ce7971d049bd12ada6f4d7d48076ca503a79
ryanschuttig <ryanschuttig@gmail.com>
2020-06-23 20:40:13 -0400

    API works, created HTML elements

---
+++ script.js
@@ -0,0 +1,27 @@
+$("button").on("click", function(event) {
+  event.preventDefault();
+
+var searchInput = $("#citysearch").val();
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=afc59b5a12ec3abd2ca379b25eda055d";
+
+var today = new Date();
+
+var date = (today.getMonth()+1)+"-"+today.getDate()+"-"+today.getFullYear();
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+  .then(function(response) {
+    console.log(queryURL);
+
+    console.log(response);
+
+    if (searchInput != null) {
+      $("#citydata").text(response.name + " " + date);
+    }
+
+  });
+});
